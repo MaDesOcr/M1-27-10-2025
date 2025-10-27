3d9ff7fbb4eda89f4416044e9bb0e42bc261a54e
MaDesOcr <madesocr@gmail.com>
2025-10-27 12:32:02 +0100

    perso

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,24 @@
-import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack, useRouter } from "expo-router";
 
 export default function RootLayout() {
+    const router = useRouter();
+  
   return (
     <Stack>
       <Stack.Screen name="page1"/>
-      <Stack.Screen name="(page2)"/>
+      <Stack.Screen name="(page2)" options={{
+        title:"",
+         //headerTitle:()=><Ionicons name="home"></Ionicons>,
+         headerLeft:({...props})=><Ionicons {...props}
+         name="home">
+          
+         </Ionicons>
+      }}/>
     </Stack>
   )
 }
+
+
+//onPress={()=> router.back() }>
+         
