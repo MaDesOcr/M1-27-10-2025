adfbcd321b87308a68c327798b2e1d01657e9f77
MaDesOcr <madesocr@gmail.com>
2025-10-27 11:53:39 +0100

    button

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,11 @@
 import { Link, useRouter } from "expo-router";
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, StyleSheet } from "react-native";
 
 export default function Index() {
   const router = useRouter();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={style.viewStyle}>
+
       <Button onPress={()=>router.push("/page1")} 
       title="Page 1"/>
       <Button onPress={()=>router.push("/tab1")} 
@@ -18,7 +13,18 @@ export default function Index() {
 
      {/* <Link href="/page1">PAGE1</Link>
       <Link href="/tab1">PAGE2</Link>*/}
-      
+
     </View>
   );
 }
+
+  const style = StyleSheet.create({
+    viewStyle:{flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "row",
+      gap:"10%",
+      flexBasis: "auto" //gap:10 =>px
+    }
+
+})
