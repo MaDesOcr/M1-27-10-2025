bdcc8696d8033a770b9d1ed74c6e0503acc5d720
MaDesOcr <madesocr@gmail.com>
2025-10-27 13:45:18 +0100

    add component

--- app/page1.tsx
+++ app/page1.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Text, View } from "react-native";
+import DisplayDate from "@/component/DisplayDate";
 
 export default function page1(){
     return(
         <View >
             <Text> BONJOUR </Text>
+            <DisplayDate/>
+                        <DisplayDate/>
+            <DisplayDate/>
+            <DisplayDate/>
+            <DisplayDate/>
+
         </View>
     )
 }
