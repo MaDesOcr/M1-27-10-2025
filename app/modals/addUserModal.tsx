0ea826001c51b29b9a56fd23000930277d92f0df
MaDesOcr <madesocr@gmail.com>
2025-10-28 16:03:14 +0100

    add user fonctionel

--- app/modals/addUserModal.tsx
+++ app/modals/addUserModal.tsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
-import { Text, TextInput } from "react-native";
+import { Button, TextInput } from "react-native";
+import { useRouter } from "expo-router";
 
-export default function addUserModal(){
+export default function addUserModal() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
-    
-    
+    const router = useRouter();
+
+    async function addUser() {
+        const persoAdded = await fetch('https://dummyjson.com/users/add',
+            {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    firstName,
+                    lastName
+                })
+            })
+            console.log(persoAdded);
+            router.back();
+    }
+
     return (
         <>
             <TextInput value={firstName} onChangeText={setFirstName}></TextInput>
             <TextInput value={lastName} onChangeText={setLastName}></TextInput>
-            
-        </>       
+            <Button title="Add" onPress={addUser}></Button>
+        </>
     )
 }
