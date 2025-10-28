ccd4ceaa8ceb12341efba059f499f79e1831314d
MaDesOcr <madesocr@gmail.com>
2025-10-28 14:04:13 +0100

    pageDetails fonctionel

--- app/pageDetail.tsx
+++ app/pageDetail.tsx
@@ -2,19 +2,25 @@ import { getPersonDetails } from "@/service/PersonnageService";
 import { useState, useEffect } from "react";
 import { Personnage } from "@/model/Personnage";
 import { useLocalSearchParams, useSearchParams } from "expo-router/build/hooks";
+import FichePerso from "@/component/FichePerso";
 
 export default function pageDetail(){
 
     const {id} = useLocalSearchParams<{id : string}>()
 
     const [perso, setPerso] = useState<Personnage>();
+
     useEffect(() => {
             getPersonDetails(parseInt(id)).then((data) => setPerso(data))
         }, [])
 
+    if(!perso){
+        return
+    }
+
     return(
         <>
-            {JSON.stringify(perso)}
+        {perso && <FichePerso {...perso}/>}
         </>
     )
 }
